e0501fa731665179c9847aad420f41340fe1159c
Harrison Goldwyn <harrison.goldwyn@nrel.gov>
2025-05-26 14:23:23 -0600

    Bug in sf 10

--- sr_ordered_train0p75/stage_3/fcov_GL_mseStart_gPAStdOn10.py
+++ sr_ordered_train0p75/stage_3/fcov_GL_mseStart_gPAStdOn10.py
@@ -15,8 +15,8 @@ from dlkit.opt.train import train_epochs
 from dlkit.nets.conv2d import Conv2dUpscaleModelInterpolate
 
 ## Load model module
-sys.path.append('/Users/hgoldwyn/Research/projects/SR_CNN/sr_cnn/synthetic_data/torch_models') #TODO adjust path
-sys.path.append('/projects/ecrpstats/sr_cnn/synthetic_data/torch_models')
+sys.path.append('/Users/hgoldwyn/Research/projects/SR_CNN/paper_repo/auxilary_modules') #TODO adjust path
+sys.path.append('/projects/ecrpstats/distributional_SRCNN/auxilary_modules')
 import data_loading
 import plotting
 
@@ -41,6 +41,8 @@ batch_size = 32
 ##
 region = 0
 subregion = 'all'
+##
+scale_factor_str = "10"
 
 try:
     log_file_base_path = os.path.basename(__file__)[:-3]
@@ -52,52 +54,12 @@ save_path = f'{log_file_base_path}.pt'
 
 ## ~~~~~~~~~~~
 ## import data
-try:
-    data_hr = np.load("/Users/hgoldwyn/Research/projects/SR_CNN/paper_repo/data/subregions_wind_u_64x64.npy")
-    data_lr = np.load("/Users/hgoldwyn/Research/projects/SR_CNN/paper_repo/data/subregions_wind_u_8x8_downscaled64x64.npy")
-except:
-    pass
-try:
-    data_hr = np.load("/projects/ecrpstats/distributional_SRCNN/data/subregions_wind_u_64x64.npy")
-    data_lr = np.load("/projects/ecrpstats/distributional_SRCNN/data/subregions_wind_u_8x8_downscaled64x64.npy")
-except:
-    pass
-
-if type(subregion) == int:
-    data_hr = data_hr[region, subregion]
-    data_lr = data_lr[region, subregion]
-elif subregion == 'all':
-    
-    ## To order by timeshape, each 4 images being the 4 subregions
-    data_hr = data_hr[region]
-    data_lr = data_lr[region]
-    # Transpose so axis 0 (4) comes after axis 1 (214)
-    data_hr = np.transpose(data_hr, (1, 0, 2, 3))  # shape (214, 4, 64, 64)
-    data_lr = np.transpose(data_lr, (1, 0, 2, 3)) 
-    # Now reshape to (214*4, 64, 64)
-    data_hr = data_hr.reshape((-1, 64, 64))  # shape (856, 64, 64)
-    data_lr = data_lr.reshape((-1, 8, 8))
-else:
-    raise ValueError("subregion invalid")
-
-# %%
-train_set_size = int(data_hr.shape[0] * .7)
-test_set_size = data_hr.shape[0] - train_set_size
-
-# %%
-## Normalize data
-raw_std = data_hr.std()
-data_hr = data_hr / raw_std
-rescaled_mean = data_hr.mean()
-data_hr = data_hr - rescaled_mean
-data_lr = data_lr / raw_std - rescaled_mean
-
-# %%
-## Get train and test sets
-xtrainHR = data_hr[:train_set_size].astype(np.float32)[:, None, :, :] 
-xtestHR = data_hr[train_set_size:train_set_size+test_set_size].astype(np.float32)[:, None, :, :] 
-xtrainLR = data_lr[:train_set_size].astype(np.float32)[:, None, :, :] 
-xtestLR = data_lr[train_set_size:train_set_size+test_set_size].astype(np.float32)[:, None, :, :]         
+xtrainHR, xtestHR, xtrainLR, xtestLR = data_loading.import_data(
+    region,
+    subregion,
+    train_fraction=.75,
+    order='(subregion, time)'
+    )
 
 # set device
 device = (
@@ -173,7 +135,7 @@ scheduler = ExponentialDecayWithFloor(optimizer, decay_rate=0.95, floor=0.0001)
 # except:
 #     pass
 try:
-    fit_params = np.load("/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_2/parCov_fitting_fourier_allImages_mseStart_globalPrior_anal_empPriorStdOn10_param_fits.npy")
+    fit_params = np.load(f"/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_2/parCov_fitting_fourier_allImages_mseStart_globalPrior_anal_empPriorStdOn{scale_factor_str}_param_fits.npy")
 except:
     pass
 

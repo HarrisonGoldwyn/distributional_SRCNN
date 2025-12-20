6d710b874de264368b5986219c2fe50c9cad9201
Harrison Goldwyn <harrison.goldwyn@nrel.gov>
2025-12-20 14:11:43 -0700

    fixed output paths in stage 2

--- sr_ordered_train0p75/stage_2/parCov_fitting_fourier_allImages_mseStart_globalPrior_anal_empPriorStdOn1.py
+++ sr_ordered_train0p75/stage_2/parCov_fitting_fourier_allImages_mseStart_globalPrior_anal_empPriorStdOn1.py
@@ -62,8 +62,8 @@ print(f"Using device: {device}")
 # except:
 #     pass
 try:
-    #     test_mse_error_fields = np.load("/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_1/mse_5l_i123_c32s_padR_schLrG0p95_reg0_errFields.npy")
-    train_mse_error_fields = np.load("/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_1/mse_5l_i123_c32s_padR_schLrG0p95_reg0_TrainErrFields.npy")
+    #     test_mse_error_fields = np.load("/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_1/output_data/mse_5l_i123_c32s_padR_schLrG0p95_reg0_errFields.npy")
+    train_mse_error_fields = np.load("/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_1/output_data/mse_5l_i123_c32s_padR_schLrG0p95_reg0_TrainErrFields.npy")
 except:
     pass
 
@@ -124,14 +124,14 @@ basis_functions_tensor = torch.tensor(basis_functions, dtype=torch.complex64).to
 ##
 ## Load parameters for global prior
 try:
-    global_fit_params = np.load("/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_2/anal_sln_global_params.npy")
+    global_fit_params = np.load("/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_2/output_2a/anal_sln_global_params.npy")
 except:
     pass
 
 global_fit_params = torch.tensor(global_fit_params, dtype=torch.float32).to(device)
 
 ## Load emperical standard deviation for parameter prior
-global_fit_param_stdd = np.load("/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_2/img_spec_params_std.npy")
+global_fit_param_stdd = np.load("/projects/ecrpstats/distributional_SRCNN/sr_ordered_train0p75/stage_2/output_2a/img_spec_params_std.npy")
 global_fit_param_stdd = torch.tensor(global_fit_param_stdd).to(device)
 
 # %%
